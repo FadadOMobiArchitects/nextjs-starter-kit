33d9d7c43ef455287e434272089b7e697e84b120
FadadOMobiArchitects <oussama.f@mobiarchitects.com>
2025-10-07 14:19:47 +0100

    feat: implement HomePage component with internationalization support

---
+++ src/app/[locale]/(core)/home/page.tsx
@@ -0,0 +1,7 @@
+import { getTranslations } from "next-intl/server";
+
+export default async function HomePage() {
+  const t = await getTranslations("HomePage");
+
+  return <h1>{t("title")}</h1>;
+}
