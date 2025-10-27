309d0f0910b50b87c3de89cd89ae68cd10ec59d2
FadadOMobiArchitects <oussama.f@mobiarchitects.com>
2025-10-27 16:03:16 +0100

    feat: enhance CoreLayout by adding SidebarProvider and AppSidebar for improved layout structure

--- src/app/[locale]/(core)/layout.tsx
+++ src/app/[locale]/(core)/layout.tsx
@@ -1,9 +1,20 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { ReactNode } from "react";
+import { AppSidebar } from "./_components/app-sidebar";
+import AppNavbar from "./_components/app-navbar";
 
 export default async function CoreLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  return <div className="relative w-full overflow-x-hidden">{children}</div>;
+  return (
+    <SidebarProvider defaultOpen={false}>
+      <AppSidebar />
+      <div className="relative w-full overflow-x-hidden">
+        <AppNavbar />
+        {children}
+      </div>
+    </SidebarProvider>
+  );
 }
