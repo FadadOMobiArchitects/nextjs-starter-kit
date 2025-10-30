c017fe029da7baa5dd642b73f18d7f8585e3112c
FadadOMobiArchitects <oussama.f@mobiarchitects.com>
2025-10-30 12:18:56 +0100

    feat: implement CorePage component with user fetching and language selection

--- src/app/[locale]/(core)/page.tsx
+++ src/app/[locale]/(core)/page.tsx
@@ -2,28 +2,28 @@ import { LanguageSelector } from "@/components/generics/language-selector";
 import { fetchUsers } from "@/queries/users-queries";
 import { getTranslations } from "next-intl/server";
 
-export default async function HomePage() {
-  const t = await getTranslations("HomePage");
+export default async function CorePage() {
+  const t = await getTranslations("CorePage");
   const { users } = await fetchUsers();
 
   return (
     <div className="case text-center space-y-4">
-      <div className="absolute top-4 right-4">
-        <LanguageSelector align="end" />
-      </div>
-
-      <div>
+      <div className="relative">
         <h1 className="text-base">{t("title")}</h1>
         <p className="text-sm text-muted-foreground">{t("description")}</p>
       </div>
 
-      <ul>
+      <ul className="relative">
         {users.map((user: any) => (
           <li key={user.id}>
             {user.firstName} - {user.lastName}
           </li>
         ))}
       </ul>
+
+      <div className="mx-auto w-fit">
+        <LanguageSelector />
+      </div>
     </div>
   );
 }
