7ab7c9d47ff6e7067935e876db29da0f942b346e
FadadOMobiArchitects <oussama.f@mobiarchitects.com>
2025-10-30 17:32:46 +0100

    feat: update home page with multi-lang integration

--- src/app/[locale]/(core)/page.tsx
+++ src/app/[locale]/(core)/page.tsx
@@ -1,11 +1,25 @@
+import { LanguageSelector } from "@/components/common/language-selector";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard } from "lucide-react";
 import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 
 export default async function CorePage() {
   const t = await getTranslations("CorePage");
 
   return (
-    <div className="case text-center space-y-4">
+    <div className="case flex flex-col items-center justify-center min-h-screen space-y-2">
+      <div className="absolute top-4 right-4 z-50">
+        <LanguageSelector align="end" />
+      </div>
+
       <h1 className="text-lg font-medium">{t("title")}</h1>
+
+      <Link href="/dashboard">
+        <Button startIcon={<LayoutDashboard strokeWidth={1.75} />}>
+          Access The Dashboard
+        </Button>
+      </Link>
     </div>
   );
 }
